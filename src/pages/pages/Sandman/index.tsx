91fc4e8df251e4fe53e1c87700124d561f014f32
Enrique <enriqueferreira956@gmail.com>
2023-08-24 00:37:45 -0300

    0.0.2

--- src/pages/pages/Sandman/index.tsx
+++ src/pages/pages/Sandman/index.tsx
@@ -1,13 +1,21 @@
+import  useStore  from "../../../store/useStore"
 import ModalView from "../../layout/Modal/ModalView"
 import useIncreaseSkill from "./hooks/useIncreaseSkill"
 
 export function SandmanView(){
     const increaseSkill = useIncreaseSkill()
+    const [toJson, toEncoded, toDecoded] = useStore((state) => [state.toJson, state.toEncoded, state.toDecoded])
+    const [storage, storageEncoded] = useStore((state) => [state.storage, state.storageEncoded])
 
     return(
         <ModalView>
             <section className="flex items-center flex-col gap-10 pt-10">
                 <button onClick={() => increaseSkill(1)} className="bg-green-800 p-4 w-[20rem] rounded-xl">Do a single sword moviment</button>
+                <button onClick={() => toJson()}>toJSon</button>
+                <button onClick={() => toEncoded()}>toEncoded</button>
+                <button onClick={() => toDecoded()}>toDecoded</button>
+                <button onClick={() => console.log(storage)}>console.log</button>
+                <button onClick={() => console.log(storageEncoded)}>console.log2</button>
             </section>
         </ModalView>
     )
--- src/store/useStore.tsx
+++ src/store/useStore.tsx
@@ -2,11 +2,21 @@ import { create } from 'zustand'
 
 interface StoreState {
   skill: number
+  storage: string
+  storageEncoded: string
   increase: (by: number) => void
-}
+  toJson: () => void
+  toEncoded: () => void
+  toDecoded: () => void
+  }
 
 const useStore = create<StoreState>()((set) => ({
   skill: 0,
+  storage: '',   
+  storageEncoded: '',
+  toJson: () => set((state) => ({storage: JSON.stringify({'skill': '' + state.skill})})),
+  toEncoded: () => set((state) => ({storageEncoded: btoa(state.storage)})),
+  toDecoded: () => set((state) => ({storage: atob(state.storageEncoded)})),
   increase: (by) => set((state) => ({ skill: state.skill + by })),
 }))
 
